64a93d0c70dc9943f30359667d19b76a887ec9f0
Kailashchandras <kailash@j2apps.com>
2019-04-10 18:28:42 +0530

    Modified Lambda function for Thank you and Error page

--- penstormLambda.js
+++ penstormLambda.js
@@ -2,20 +2,25 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const querystring = require('querystring');
 
+
 exports.handler = (event, context, callback) => {
-    console.log(event.body);
     
-    const params = querystring.parse(event.body);
+    const data = JSON.parse(JSON.stringify(event))
+    
+ 
+    
 
     // Our field from the request.
-    const email = params['email'];
-    const name = params['name'];
-    const phone = params['phone'];
-    const genre = params['genre'];
-    const category = params['category'];
-    const message = params['message'];
+    const email = data.email;
+    const name = data.name;
+    const phone = data.phone;
+    const genre = data.genre;
+    const companyName = data.companyname;
+    const projecttitle = data.projecttitle;
+    const category = data.category;
+    const message = data.message;
     dynamodb.putItem({
-        TableName: "penstorm",
+        TableName: "pen-storm",
         Item: {
             "email": {
                 S: email
@@ -25,6 +30,12 @@ exports.handler = (event, context, callback) => {
             },
             "phone": {
                 S: phone
+            },
+            "companyname": {
+                S: companyName
+            },
+            "projecttitle": {
+                S: projecttitle
             },
             "genre": {
                 S: 'genre'
@@ -41,13 +52,12 @@ exports.handler = (event, context, callback) => {
             console.log(err, err.stack);
             callback(null, {
                 statusCode: '500',
-                body: err
+                body: '<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"></head><body><div class="jumbotron text-xs-center"> <h1 class="display-3">Some Internal Error. Please try again Later </h1>  <hr> <p> Having trouble? Write to us on <a href="">penstorm@mxplayer.in</a> </p> <p class="lead"> <a class="btn btn-primary btn-sm" href="http://penstorm.mxplayer.in" role="button">Continue to homepage</a> </p></div>'
             });
         } else {
-            callback(null, {
-                statusCode: '200',
-                body: 'Your Registration is Successful '  + '!' + ' See you at Venue'
-            });
+            
+            context.succeed('<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"></head><body><div class="jumbotron text-xs-center"> <h1 class="display-3">Thank You!</h1> <p class="lead"><strong>Your Registration is Successful</strong>. See you at Venue</p> <hr> <p> Having trouble? Write to us on <a href="">penstorm@mxplayer.in</a> </p> <p class="lead"> <a class="btn btn-primary btn-sm" href="http://penstorm.mxplayer.in" role="button">Continue to homepage</a> </p></div>');
+            
         }
     })
 };
