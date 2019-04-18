9da1bb3184792748db6ba1d622d8a51fe220c69c
Kailashchandras <kailash@j2apps.com>
2019-04-18 23:26:54 +0530

    Changes done as per marketing team suggestion

--- penstormLambda.js
+++ penstormLambda.js
@@ -4,21 +4,33 @@ const querystring = require('querystring');
 
 
 exports.handler = (event, context, callback) => {
-    
-    const data = JSON.parse(JSON.stringify(event))
-    
  
+    const data = JSON.parse(JSON.stringify(event))
+    console.log(data);
     
-
-    // Our field from the request.
     const email = data.email;
     const name = data.name;
+    
     const phone = data.phone;
+    
     const genre = data.genre;
-    const companyName = data.companyname;
+    
+    let camp_name = "NA";
+    if(data.hasOwnProperty('companyname')){
+        console.log("Property present");
+        camp_name = data.companyname;
+        
+    }
+    else{
+        console.log("Property NOT present");
+        camp_name = "NA";
+    }
+    
+    const companyName = "NAME";
     const projecttitle = data.projecttitle;
     const category = data.category;
     const message = data.message;
+    
     dynamodb.putItem({
         TableName: "pen-storm",
         Item: {
@@ -32,16 +44,16 @@ exports.handler = (event, context, callback) => {
                 S: phone
             },
             "companyname": {
-                S: companyName
+                S: 'companyName'
             },
             "projecttitle": {
                 S: projecttitle
             },
             "genre": {
-                S: 'genre'
+                S: genre
             },
             "category": {
-                S: 'category'
+                S: category
             },
             "message": {
                 S: message
@@ -56,7 +68,7 @@ exports.handler = (event, context, callback) => {
             });
         } else {
             
-            context.succeed('<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"></head><body><div class="jumbotron text-xs-center"> <h1 class="display-3">Thank You!</h1> <p class="lead"><strong>Your Registration is Successful</strong>. See you at Venue</p> <hr> <p> Having trouble? Write to us on <a href="">penstorm@mxplayer.in</a> </p> <p class="lead"> <a class="btn btn-primary btn-sm" href="http://penstorm.mxplayer.in" role="button">Continue to homepage</a> </p></div>');
+            context.succeed('<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"></head><body><div class="jumbotron text-xs-center"> <h1 class="display-3">Thank You!</h1> <p class="lead"><strong>Your Registration is Successful</strong>. </p> <hr> <p> Having trouble? Write to us on <a href="mailto:penstorm@mxplayer.in" target="_top"><p class="display-7">penstorm@mxplayer.in</p></a> </p> <p class="lead"> <a class="btn btn-primary btn-sm" href="http://penstorm.mxplayer.in" role="button">Continue to homepage</a> </p></div>');
             
         }
     })
